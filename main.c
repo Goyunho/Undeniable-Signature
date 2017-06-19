e1d893664c5f54e111f0d5a0a36ae4f0960fb3fa
Goyunho <rhdbsgh0629@naver.com>
2017-06-20 06:57:23 +0900

    add comments

--- main.c
+++ main.c
@@ -2,7 +2,6 @@
 #include <stdlib.h>
 #include <time.h>
 
-
 #define ll long long
 
 // alice and bob asets
@@ -22,11 +21,13 @@ int z = 0; // signature
 int m = 0; // message
 int y = 0; // public key
 
+
 // users asets
 Alice alice = {0,};
 Bob bob = {0,};
 
-int powmod(int p, int n, int m) {
+
+int powmod(int p, int n, int m) { // pow and modular
     int result = 1;
 
     while(n--)
@@ -36,7 +37,7 @@ int powmod(int p, int n, int m) {
 }
 
 
-int mul_inv(int a, int b) {
+int mul_inv(int a, int b) { // euclide algorithm
 	int b0 = b, t, q;
 	int x0 = 0, x1 = 1;
 	if (b == 1) return 1;
@@ -66,14 +67,14 @@ int is_prime(int n) { // prime is 1, no prime is 0
 }
 
 
-int gcd(int a, int b) {
+int gcd(int a, int b) { // gcd check
 	return b ? gcd(b, a%b) : a;
 }
 
 
-void key_setup() {
+void key_setup() { // Alices setup key
     do {
-    printf("input prime number p : "); scanf("%d", &p);
+        printf("input prime number p : "); scanf("%d", &p);
     } while(!is_prime(p));
 
     do {
@@ -85,7 +86,8 @@ void key_setup() {
     y = powmod(g, alice.x, p); // make public key
 }
 
-int signing(int m) {
+
+int signing(int m) { // Alice's signing
     key_setup();
     printf("g : %d\n", g);
     printf("m : %d, x : %d\n", m, alice.x);
@@ -97,51 +99,51 @@ int signing(int m) {
 }
 
 
-int bob_computes_c() {
+int bob_computes_c() { // Bob computes c
     int a, b; // random numbers
     int c; // chiper
 
-    bob.a = rand()%p;
+    bob.a = rand()%p; // choose random a, b
     bob.b = rand()%p;
 
     printf("a: %d, b: %d\n", bob.a, bob.b);
-    c = (powmod(z, bob.a, p)*powmod(y, bob.b, p)%p);
+    c = (powmod(z, bob.a, p)*powmod(y, bob.b, p)%p); // computes c
 
     return c;
 }
 
 
-int alice_computes_d(int c) {
+int alice_computes_d(int c) { // Alice computes d whith c
     int d; // decode
     int xi; // inverse x
 
-    xi = mul_inv(alice.x, p-1);
+    xi = mul_inv(alice.x, p-1); // get inverse x
     printf("xi : %d\n", xi);
-    d = powmod(c, xi, p);
+    d = powmod(c, xi, p); // computes d
 
     return d;
 }
 
 
-int bob_confirms(int d_a) {
+int bob_confirms(int d_a) { // Bob confirms d computed by Alice and Bob
     int d_b; // made by bob
 
-    d_b = (powmod(m, bob.a, p)*powmod(g, bob.b, p))%p;
+    d_b = (powmod(m, bob.a, p)*powmod(g, bob.b, p))%p; // computes d
     printf("Bob computed d : %d\n", d_b);
 
     return d_a==d_b?1:0;
 }
 
 
-int verifying() {
+int verifying() { // verifying process
     int c; // chiper
     int d; // decode
 
-    c = bob_computes_c();
+    c = bob_computes_c(); // Bob made c
     printf("c : %d\n", c);
-    d = alice_computes_d(c);
+    d = alice_computes_d(c); // Alice made d from c
     printf("Alice return d : %d\n", d);
-    if(bob_confirms(d)) {
+    if(bob_confirms(d)) { // Bob confirms Alice's d and Bob's d
         printf("This signature is true!\n");
     }
     else {
@@ -156,12 +158,12 @@ int main(int argc, char* argv[]) {
         return 1;
     }
 
-    srand(time(NULL));
+    srand(time(NULL)); // random seed is timestamp
 
-    m = atoi(argv[1]);
+    m = atoi(argv[1]); // message
 
-    signing(m);
-    verifying();
+    signing(m); // signing
+    verifying(); // verifying
 
     return 0;
 }
